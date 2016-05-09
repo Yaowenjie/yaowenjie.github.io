86e0c55c46ed15bb107d1078b06a611cdf0ff668
Yaowenjie <wjyao@thoughtworks.com>
2016-05-09 13:13:52 +0800

    Add test task in gradle and add simple selenium unit test

--- cucumber-test/src/test/java/com/wenjie/HomePageTest.java
+++ cucumber-test/src/test/java/com/wenjie/HomePageTest.java
@@ -1,15 +1,24 @@
 package com.wenjie;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HomePageTest {
     FirefoxDriver driver = new FirefoxDriver();
 
     @Test
-    public void shouldClickMenuAndSearchButton() {
+    public void shouldClickSearchButtonAndGoToAnArticle() throws InterruptedException {
         driver.get("http://localhost:4000");
+        driver.findElement(By.xpath("//*[@id=\"masthead\"]/button")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div/input")).sendKeys("Powershell");
+        driver.findElement(By.xpath("/html/body/div[1]/div/ul/li[1]/article/a")).click();
+        String articleTitle = driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/article/header/h1")).getText();
+        assertThat(articleTitle.toLowerCase(), containsString("Powershell".toLowerCase()));
+        driver.close();
     }
-
 
 }
