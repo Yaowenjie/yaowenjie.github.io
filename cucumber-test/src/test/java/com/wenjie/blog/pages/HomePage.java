f413cd2fe932b13f880b0bef39ac6bf36e4134ec
Yaowenjie <wjyao@thoughtworks.com>
2016-05-11 13:53:06 +0800

    Add some webelements on homepage

--- cucumber-test/src/test/java/com/wenjie/blog/pages/HomePage.java
+++ cucumber-test/src/test/java/com/wenjie/blog/pages/HomePage.java
@@ -4,6 +4,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
+    @FindBy(id = "masthead")
+    private WebElement header;
+
+    @FindBy(id = "footer")
+    private WebElement footer;
+
+    @FindBy(id = "slide")
+    private WebElement menuButton;
+
+    @FindBy(xpath = "//*[@id=\"navigation\"]/li[1]/a")
+    private WebElement homeLink;
+
+    @FindBy(xpath = "//*[@id=\"navigation\"]/li[2]/a")
+    private WebElement categoriesLink;
+
+    @FindBy(xpath = "//*[@id=\"navigation\"]/li[3]/a")
+    private WebElement tagsLink;
+
+    @FindBy(xpath = "//*[@id=\"navigation\"]/li[4]/a")
+    private WebElement aboutAuthorLink;
+
+    @FindBy(xpath = "//*[@id=\"navigation\"]/li[5]/a")
+    private WebElement messageBoardLink;
+
     @FindBy(xpath = "//*[@id=\"masthead\"]/button")
     private WebElement searchButton;
 
@@ -13,12 +37,13 @@ public class HomePage {
     @FindBy(xpath = "/html/body/div[1]/div/ul/li[1]/article/a")
     private WebElement searchResultFirst;
 
-    @FindBy(id = "masthead")
-    private WebElement header;
+    @FindBy(xpath = "//*[@id=\"main\"]/div/div/div/div[1]/article/div/div/a")
+    private WebElement firstReadMoreButton;
 
-    @FindBy(id = "footer")
-    private WebElement footer;
+    @FindBy(xpath = "//*[@id=\"main\"]/div/div/div/div[6]/div/nav/a")
+    private WebElement OlderPostButton;
 
+    
     public void searchKeywordAndEnterPost(String wantedStr){
         searchButton.click();
         searchInput.sendKeys(wantedStr);
--- cucumber-test/src/test/resources/features/baseflow.feature
+++ cucumber-test/src/test/resources/features/baseflow.feature
@@ -9,10 +9,11 @@ Feature: Test home page
 
   Scenario:
     Given I enter my blog address "http://localhost:4000" and go to Home page
-    # Random Background Image
+    # Random Background Image:
     And I can see the page header and footer have same backgroud image
     Then I refresh current page to see the backgroud image changes
-    # Search Feature
+    # Search Button Feature:
     When I click search button and enter "Powershell" in the input field and click the first result
     Then I go to the article page with title containing "Powershell"
     Then I close current window
+    # Post Page Feature:
